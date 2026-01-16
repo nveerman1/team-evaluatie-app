db226f8e5b5c51ad634d6345af178731138807a6
nveerman1 <nveerman@melanchthon.nl>
2026-01-16 15:16:46 +0100

    alembic merge migration

--- backend/migrations/versions/adec74c3bac7_merge_heads.py
+++ backend/migrations/versions/adec74c3bac7_merge_heads.py
@@ -9,10 +9,9 @@ Create Date: 2026-01-16 15:13:18.223791
 from typing import Sequence, Union
 
 
-
 # revision identifiers, used by Alembic.
 revision: str = "adec74c3bac7"
-down_revision: Union[str, None] = ("seed_20260111_01", "pasa_20260116_01")
+down_revision: Union[str, Sequence[str], None] = ("seed_20260111_01", "pasa_20260116_01")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
