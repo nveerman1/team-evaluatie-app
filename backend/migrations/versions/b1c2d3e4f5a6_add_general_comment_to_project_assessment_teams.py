29fa120246b41a942d72ca26662a271a7c5a0057
nveerman1 <nveerman@melanchthon.nl>
2026-03-23 12:57:35 +0100

    correct migration down revision

--- backend/migrations/versions/b1c2d3e4f5a6_add_general_comment_to_project_assessment_teams.py
+++ backend/migrations/versions/b1c2d3e4f5a6_add_general_comment_to_project_assessment_teams.py
@@ -1,7 +1,7 @@
 """add_general_comment_to_project_assessment_teams
 
 Revision ID: b1c2d3e4f5a6
-Revises: a1b2c3d4e5f6
+Revises: a2b3c4d5e6f7
 Create Date: 2026-03-23 11:00:00.000000
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "b1c2d3e4f5a6"
-down_revision = "a1b2c3d4e5f6"
+down_revision = "a2b3c4d5e6f7"
 branch_labels = None
 depends_on = None
 
